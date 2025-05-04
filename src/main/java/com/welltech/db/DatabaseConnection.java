1cc6bc450d5e07f0c8b184109b975033f1bdb8c9
Mehdi Bougataya <bougattayamehdi1@gmail.com>
2025-05-05 01:12:22 +0200

    work in progress db password

--- src/main/java/com/welltech/db/DatabaseConnection.java
+++ src/main/java/com/welltech/db/DatabaseConnection.java
@@ -11,7 +11,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/pi";
     private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "";
 
     /**
      * Get a connection to the database
